1d642a44c5210efa60f285b67e49afd15b3b2cc5
Aown Ali <aownali254@gmail.com>
2024-11-17 17:09:12 +0500

    Task 2 completed

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,21 +1,24 @@
+import { IncomingHttpHeaders } from "http";
 import { LoginDto } from "./dtos/login.dto";
 import { SignupDto } from "./dtos/signup.dto";
+import { AuthService } from "./auth.service";
 
+// Services
+const authService = AuthService.getInstance();
+
+// Resolver
 export const resolver = {
-   Mutation: {
-      signup: (_: never, args: SignupDto) => {
-         console.log("args", args);
-         return {
-            token: "token...",
-            role: "role...",
-         };
+   Query: {
+      switchRole: (_: undefined, __: undefined, headers: IncomingHttpHeaders) => {
+         return authService.switchRole(headers.authorization, headers.origin);
       },
-      login: (_: undefined, args: LoginDto) => {
-         console.log("args", args);
-         return {
-            token: "token...",
-            role: "role...",
-         };
+   },
+   Mutation: {
+      signup: async (_: undefined, args: SignupDto, headers: IncomingHttpHeaders) => {
+         return authService.signup(args, headers.origin);
+      },
+      login: (_: undefined, args: LoginDto, headers: IncomingHttpHeaders) => {
+         return authService.login(args, headers.origin);
       },
    },
 };
--- src/auth/jwt.service.ts
+++ src/auth/jwt.service.ts
@@ -1,54 +1,50 @@
-import { Algorithm, sign, verify } from 'jsonwebtoken'
-import { Role } from '../utilities/types/user'
+import { Algorithm, sign, verify } from "jsonwebtoken";
+import { Role } from "../utilities/types/user";
 
 export class JwtService {
-    private secret = process.env.JWT_SECRET as string
-    private issuer = process.env.JWT_ISSUER as string
-    private expiresIn = process.env.JWT_EXPIRATION as string
-    private algorithm: Algorithm = 'HS384'
+   private secret = process.env.JWT_SECRET as string;
+   private issuer = process.env.JWT_ISSUER as string;
+   private expiresIn = process.env.JWT_EXPIRATION as string;
+   private algorithm: Algorithm = "HS384";
 
-    getToken(audience: string, userId: string, email: string, role: Role = "buyer") {
-        const payload = {id: userId, email, role}
+   getToken(audience: string, userId: string, email: string, role: Role = "buyer") {
+      const payload = { id: userId, email, role };
 
-        const token = sign(payload, this.secret, {
-            audience,
-            issuer: this.issuer,
-            expiresIn: this.expiresIn,
-            algorithm: this.algorithm,
-        })
+      const token = sign(payload, this.secret, {
+         audience,
+         issuer: this.issuer,
+         expiresIn: this.expiresIn,
+         algorithm: this.algorithm,
+      });
 
-        console.log('token',token)
-        return token
-    }
+      return token;
+   }
 
-    verifyToken(jwtToken: string, audience: string) {
-        const payload = verify(jwtToken, this.secret, {
-            algorithms: [this.algorithm], 
-            issuer: this.issuer,
-            audience, 
-        })
+   verifyToken(jwtToken: string, audience: string) {
+      const payload = verify(jwtToken, this.secret, {
+         algorithms: [this.algorithm],
+         issuer: this.issuer,
+         audience,
+      });
 
-        console.log('payload',payload)
-        return payload
-    }
+      return payload;
+   }
 
-    //////////////////////
-    /// Singleton pattern
-    //////////////////////
-    private static _instance: JwtService | null = null
-    private constructor() {
-        if(!this.secret || !this.expiresIn || !this.issuer) {
-            throw new Error("JWT environment variables not defined!")
-        }
-    }
+   //////////////////////
+   /// Singleton pattern
+   //////////////////////
+   private static _instance: JwtService | null = null;
+   private constructor() {
+      if (!this.secret || !this.expiresIn || !this.issuer) {
+         throw new Error("JWT environment variables not defined!");
+      }
+   }
 
-    static getInstance() {
-        if(!JwtService._instance) {
-            JwtService._instance = new JwtService()
-        }
+   static getInstance() {
+      if (!JwtService._instance) {
+         JwtService._instance = new JwtService();
+      }
 
-        return JwtService._instance
-    }
+      return JwtService._instance;
+   }
 }
-
-
